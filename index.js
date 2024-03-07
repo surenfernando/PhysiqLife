7d41f4d890626c882c140faca6a7e2a05fdd6ab6
Suren Fernando <surenfernando@Surens-MacBook-Pro.local>
2024-03-08 09:15:11 +1100

    Configure: Set view engine to EJS for templating.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
