a8eaaa5be72df318d2ae17c60ff6ac55b993641e
Suren Fernando <suren.fernando2001@gmail.com>
2024-03-08 09:29:39 +1100

    Configure: Set mongoose connection to local

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+
+
 app.get("/", (req, res) => {
   res.render("index.ejs", { 
     content: "Waiting for data..." 
