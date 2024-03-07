2352e68710d91992fd87ddc3ada9a069515d93c4
Suren Fernando <surenfernando@192.168.20.47>
2024-03-07 19:30:44 +1100

    Refactor: Updated file structure for EJS support and configured dependencies.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DiabetesTracker
-Diabetes tracker for clinicians to track diabetes records and health of patients. 
+Diabetes tracker for clinicians to track diabetes records and health of patients. Dual user (patients and clinicians).
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { 
+    content: "Waiting for data..." 
+  });
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
